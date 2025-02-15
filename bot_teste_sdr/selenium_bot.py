c69200560f544c46fb1afee8ffa71277b707da22
innovai-outlier <carreira.outlier@gmail.com>
2025-02-15 00:46:56 -0300

    Create selenium_bot.py

---
+++ bot_teste_sdr/selenium_bot.py
@@ -0,0 +1,25 @@
+# selenium_bot.py
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+def iniciar_selenium():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--user-data-dir=./chrome-data")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver.get("https://web.whatsapp.com/")
+    input("Escaneie o QR Code e pressione ENTER para continuar...")
+    return driver
+
+def enviar_mensagem(driver, mensagem):
+    input_box = driver.find_element(By.XPATH, '//div[@title="Digite uma mensagem"]')
+    input_box.send_keys(mensagem)
+    input_box.send_keys(Keys.ENTER)
+    time.sleep(3)
+
+def capturar_resposta(driver):
+    time.sleep(3)
+    mensagens = driver.find_elements(By.XPATH, '//div[contains(@class, "message-out")]//span[@class]')
+    return mensagens[-1].text if mensagens else None
